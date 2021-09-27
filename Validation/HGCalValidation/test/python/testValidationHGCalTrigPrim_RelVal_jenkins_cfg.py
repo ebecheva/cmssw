a85796956dc552f5c7b52a6006eb7235f0d7a2db
ebecheva <emilia.becheva@llr.in2p3.fr>
2021-09-27 16:49:40 +0200

    Update testValidationHGCalTrigPrim_RelVal_jenkins_cfg.py
    testing

--- Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_jenkins_cfg.py
+++ Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_jenkins_cfg.py
@@ -113,7 +113,7 @@ process.onlineSaver = cms.EDAnalyzer("DQMFileSaverOnline",
     producer = cms.untracked.string('DQM'),
     path = cms.untracked.string('./'),
 #    tag = cms.untracked.string('new'),
-    tag = cms.untracked.string('hgcal'),
+    tag = cms.untracked.string('hgcal_test'),
 )
 
 process.o = cms.EndPath(process.onlineSaver)
