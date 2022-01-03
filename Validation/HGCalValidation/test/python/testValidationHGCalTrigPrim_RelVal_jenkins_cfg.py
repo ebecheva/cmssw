ca711c9c77136325e44e270ecf4ff6453f3df839
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-01-03 15:06:12 +0100

    Test displaying new information
    Test displaying new information, PR37

--- Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_jenkins_cfg.py
+++ Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_jenkins_cfg.py
@@ -108,7 +108,7 @@ process.hgcalValidationTPG_step = cms.Path(process.runHGCALValidationTPG)
 #process.dqmSaver.workflow = '/validation/' + 'HGCAL' + '/TPG'
 #process.dqmsave_step = cms.Path(process.dqmSaver)
 
-# test 1
+# test PR37
 process.onlineSaver = cms.EDAnalyzer("DQMFileSaverOnline",
     producer = cms.untracked.string('DQM'),
     path = cms.untracked.string('./'),
