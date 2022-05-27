de2c4f6dec2f7e7098aeb05e486f4a10575407cc
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-05-27 15:58:39 +0200

    testing multiconfig validation web page

--- Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_jen_cfg.py
+++ Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_jen_cfg.py
@@ -106,7 +106,7 @@ process.hgcalValidationTPG_step = cms.Path(process.runHGCALValidationTPG)
 #process.dqmSaver.workflow = '/validation/' + 'HGCAL' + '/TPG'
 #process.dqmsave_step = cms.Path(process.dqmSaver)
 
-# NEW added test env variable LABEL PR43
+# NEW added testing multiconfig validation web page
 process.onlineSaver = cms.EDAnalyzer("DQMFileSaverOnline",
     producer = cms.untracked.string('DQM'),
     path = cms.untracked.string('./'),
